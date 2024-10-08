456e9c7b8c202edb1513f7a4ed7633e6a213c13a
David Gassner <david.gassner@gmail.com>
2024-10-08 17:29:44 +0000

    Fixes to 07_01

--- Ch07/07_01e/main.go
+++ Ch07/07_01e/main.go
@@ -2,35 +2,29 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 )
 
 func main() {
-	poodle := Dog{"Poodle", 10, "Woof!"}
-	fmt.Println(poodle)
-	fmt.Printf("%+v\n", poodle)
-	fmt.Printf("Breed: %v\nWeight: %v\n", poodle.Breed, poodle.Weight)
-
-	poodle.Speak()
-	poodle.Sound = "Arf!"
-	poodle.Speak()
-	poodle.SpeakThreeTimes()
-	poodle.SpeakThreeTimes()
+	content := "Hello from Go!"
+	file, err := os.Create("./fromString.txt")
+	checkError(err)
+	length, err := io.WriteString(file, content)
+	checkError(err)
+	fmt.Printf("Wrote a file with %v characters\n", length)
+	defer file.Close()
+	defer readFile("./fromString.txt")
 }
 
-// Dog is a struct
-type Dog struct {
-	Breed  string
-	Weight int
-	Sound  string
+func readFile(fileName string) {
+	data, err := os.ReadFile(fileName)
+	checkError(err)
+	fmt.Println("Text read from file:", string(data))
 }
 
-// Speak is how the dog speaks
-func (d Dog) Speak() {
-	fmt.Println(d.Sound)
-}
-
-// SpeakThreeTimes is how the dog speaks loudly
-func (d Dog) SpeakThreeTimes() {
-	d.Sound = fmt.Sprintf("%v %v %v", d.Sound, d.Sound, d.Sound)
-	fmt.Println(d.Sound)
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
