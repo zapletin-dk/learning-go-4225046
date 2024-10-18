ca427720c72d9fb7ff549ca5f554a05c5f9b0a07
David Gassner <david.gassner@gmail.com>
2024-10-18 18:39:20 +0000

    Finish  02_02

--- Ch02/02_02e/main.go
+++ Ch02/02_02e/main.go
@@ -10,10 +10,14 @@ func main() {
 	str2 := "jumped over"
 	str3 := "the lazy brown dog."
 	aNumber := 42
-	stringLength, err := fmt.Println(str1, str2, str3)
+
+	fmt.Println(str1, str2, str3)
+	stringLength, err := fmt.Println("The value is", aNumber)
 	if err == nil {
 		fmt.Println("String length:", stringLength)
 	}
-	fmt.Printf("Value of aNumber: %v\n", aNumber)
+
+	fmt.Printf("Value of number: %v\n", aNumber)
+
 	fmt.Printf("Data type: %T\n", aNumber)
 }
