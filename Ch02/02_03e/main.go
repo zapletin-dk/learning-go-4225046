12a01efa42171b6f83e6f8523106e87d67febb8f
David Gassner <david.gassner@gmail.com>
2024-10-18 20:00:16 +0000

    Finish 02_03

--- Ch02/02_03e/main.go
+++ Ch02/02_03e/main.go
@@ -4,13 +4,22 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Enter text: ")
-	input, _ := reader.ReadString('\n')
-	fmt.Println("You entered:", input)
+	str, _ := reader.ReadString('\n')
+	fmt.Println(str)
 
+	fmt.Print("Enter a number: ")
+	str, _ = reader.ReadString('\n')
+	f, err := strconv.ParseFloat(strings.TrimSpace(str), 64)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Value of number:", f)
+	}
 }
