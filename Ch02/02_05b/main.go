ea738ce9433bb85b33394ec671bdaafaae6509c3
David Gassner <david.gassner@gmail.com>
2024-10-21 03:37:45 +0000

    Begin 02_05

--- Ch02/02_05b/main.go
+++ Ch02/02_05b/main.go
@@ -6,6 +6,12 @@ import (
 
 func main() {
 
-	fmt.Println("Math")
+	i1, i2, i3 := 12, 45, 68
+	intSum := i1 + i2 + i3
+	fmt.Println("Integer sum:", intSum)
+
+	f1, f2, f3 := 23.5, 65.1, 76.3
+	sum := f1 + f2 + f3
+	fmt.Println("Float sum:", sum)
 
 }
