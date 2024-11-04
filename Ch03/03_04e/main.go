fa0746a0f9e6320e7528f22888f64d1809222f6e
David Gassner <david.gassner@gmail.com>
2024-11-04 21:03:25 +0000

    End 03_04

--- Ch03/03_04e/main.go
+++ Ch03/03_04e/main.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
-	fmt.Println("Slices")
+	// This is a slice
+	// var colors = []string{"Red", "Green", "Blue"}
+	var colors = make([]string, 0, 3)
+	colors = append(colors, "Red", "Green", "Blue")
+	fmt.Println(colors)
+	colors = append(colors, "Purple", "Fuschia")
+	fmt.Println(colors)
+	
+	colors = remove(colors, 1)
+	fmt.Println(colors)
+	
+	sort.Strings(colors)
+	fmt.Println(colors)
+}
+
+func remove(slice []string, i int) []string {
+	return append(slice[:i], slice[i+1:]...)
 }
