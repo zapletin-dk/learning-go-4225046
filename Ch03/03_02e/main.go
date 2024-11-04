279a28444ea67def73170947e68d34e9a5dcf01e
David Gassner <david.gassner@gmail.com>
2024-11-04 19:48:52 +0000

    End 03_02

--- Ch03/03_02e/main.go
+++ Ch03/03_02e/main.go
@@ -5,16 +5,19 @@ import (
 )
 
 func main() {
+
 	anInt := 42
-	var p = &anInt
-	fmt.Println("Value of p:", *p)
+	var p *int = &anInt
+
+	if p == nil {
+		fmt.Println("p is nil")
+	} else {
+		fmt.Println("Value of p:", *p)
+	}
 
 	value1 := 42.13
 	pointer1 := &value1
-	fmt.Println("Value 1:", *pointer1)
-
 	*pointer1 = *pointer1 / 31
-	fmt.Println("Pointer 1:", *pointer1)
-	fmt.Println("Value 1:", value1)
-
+	fmt.Println("Pointer:", *pointer1)
+	fmt.Println("Original value:", value1)
 }
