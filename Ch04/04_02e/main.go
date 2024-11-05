6f805705b7a1d950fe370f5963fe03bb73b19520
David Gassner <david.gassner@gmail.com>
2024-11-05 15:19:15 +0000

    End 04_02

--- Ch04/04_02e/main.go
+++ Ch04/04_02e/main.go
@@ -2,25 +2,45 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 	"time"
 )
 
 func main() {
-	rand.Seed(time.Now().Unix())
 
-	// fmt.Println("Day", dow)
+	weekday := time.Now().Weekday()
+	fmt.Printf("Today is %v\n", weekday)
+
+	dayNumber := int(weekday)
+	fmt.Printf("The day as a number is %v\n", dayNumber)
 
 	var result string
-	switch dow := rand.Intn(7) + 1; dow {
+	
+	switch dayNumber = 0; dayNumber {
 	case 1:
-		result = "It's Sunday!"
-		// fallthrough
+		result = "It's a Monday!"
 	case 2:
-		result = "It's Monday!"
-		// fallthrough
+		result = "It's a Tuesday!"
+	case 3:
+		result = "It's a Wednesday!"
+	case 4:
+		result = "It's a Thursday!"
+	case 5:
+		result = "It's a Friday!"
+	default: 
+		result = "It's the weekend!"
+	}
+	fmt.Println(result)
+	
+	x := 0
+	switch {
+	case x < 0:
+		result = "Less than zero"
+		fallthrough
+	case x == 0:
+		result = "Equals zero"
+		fallthrough
 	default:
-		result = "It's some other day!"
+		result = "Greater than zero"
 	}
 	fmt.Println(result)
 }
