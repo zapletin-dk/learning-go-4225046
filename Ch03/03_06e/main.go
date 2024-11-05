1911d617e380d80062c6148408084ef26edf3a51
David Gassner <david.gassner@gmail.com>
2024-11-05 14:01:25 +0000

    End 03)6

--- Ch03/03_06e/main.go
+++ Ch03/03_06e/main.go
@@ -5,16 +5,12 @@ import (
 )
 
 func main() {
-	poodle := Dog{"Poodle", 10}
+	poodle := Dog{"Poodle", 34}
 	fmt.Println(poodle)
 	fmt.Printf("%+v\n", poodle)
-	fmt.Printf("Breed: %v\nWeight: %v\n", poodle.Breed, poodle.Weight)
-	poodle.Weight = 9
-	fmt.Printf("Breed: %v\nWeight: %v\n", poodle.Breed, poodle.Weight)
 }
 
-// Dog is a struct
 type Dog struct {
-	Breed  string
+	Breed string
 	Weight int
 }
