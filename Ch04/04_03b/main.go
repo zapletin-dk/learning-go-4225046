c4ac6e0f6688a2c4fde830709561365e2429fd44
David Gassner <david.gassner@gmail.com>
2024-11-07 01:39:28 +0000

    Begin 04_03

--- Ch04/04_03b/main.go
+++ Ch04/04_03b/main.go
@@ -7,4 +7,13 @@ import (
 func main() {
 	colors := []string{"Red", "Green", "Blue"}
 	fmt.Println(colors)
+
+	// states := make(map[string]string)
+	// states["WA"] = "Washington"
+	// states["OR"] = "Oregon"
+	// states["CA"] = "California"
+	// for state, _ := range states {
+	// 	println(states[state])
+	// }
+
 }
