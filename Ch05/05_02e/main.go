3eb31700edc7fd4e13a7085939adb28e325e727c
David Gassner <david.gassner@gmail.com>
2024-11-07 19:50:48 +0000

    End 05_02

--- Ch05/05_02e/main.go
+++ Ch05/05_02e/main.go
@@ -5,32 +5,24 @@ import (
 )
 
 func main() {
-	poodle := Dog{"Poodle", 10, "Woof!"}
-	fmt.Println(poodle)
-	fmt.Printf("%+v\n", poodle)
-	fmt.Printf("Breed: %v\nWeight: %v\n", poodle.Breed, poodle.Weight)
+	dog := Dog{"Poodle", "Woof"}
+	dog.Speak()
+	dog.Sound = "Arf"
+	dog.Speak()
 
-	poodle.Speak()
-	poodle.Sound = "Arf!"
-	poodle.Speak()
-	poodle.SpeakThreeTimes()
-	poodle.SpeakThreeTimes()
+	println(dog.SpeakThreeTimes())
+
 }
 
-// Dog is a struct
 type Dog struct {
-	Breed  string
-	Weight int
-	Sound  string
+	Breed string
+	Sound string
 }
 
-// Speak is how the dog speaks
 func (d Dog) Speak() {
-	fmt.Println(d.Sound)
+	fmt.Printf("The %v says %v!\n", d.Breed, d.Sound)
 }
 
-// SpeakThreeTimes is how the dog speaks loudly
-func (d Dog) SpeakThreeTimes() {
-	d.Sound = fmt.Sprintf("%v %v %v", d.Sound, d.Sound, d.Sound)
-	fmt.Println(d.Sound)
+func (d Dog) SpeakThreeTimes() string {
+	return fmt.Sprintf("%v! %v! %v!", d.Sound, d.Sound, d.Sound)
 }
