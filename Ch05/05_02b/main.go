3637769739f56ae729ecebfa6a1469a1e6d605fe
David Gassner <david.gassner@gmail.com>
2024-11-07 19:03:19 +0000

    Begin 05_02

--- Ch05/05_02b/main.go
+++ Ch05/05_02b/main.go
@@ -5,5 +5,12 @@ import (
 )
 
 func main() {
-	fmt.Println("Loops")
+	dog := Dog{"Poodle", 35, "Woof"}
+	fmt.Printf("The %v says %v!\n", dog.Breed, dog.Sound)
+}
+
+type Dog struct {
+   Breed string
+   Weight int
+   Sound string
 }
