95ce430bb56b1ec021f12de41391585f921c63f8
David Gassner <david.gassner@gmail.com>
2024-11-08 05:33:35 +0000

    End 06_01

--- Ch06/06_01e/main.go
+++ Ch06/06_01e/main.go
@@ -5,14 +5,14 @@ import (
 	"time"
 )
 
-func say(message string, sleepDuration int) {
-	time.Sleep(1 * time.Second)
-	fmt.Println(message)
-}
-
 func main() {
 	go say("Hello from the Goroutine", 1)
 	println("Hello from Main")
 	time.Sleep(2 * time.Second)
 	println("All done")
+}
+
+func say(message string) {
+	time.Sleep(1 * time.Second)
+	fmt.Println(message)
 }
