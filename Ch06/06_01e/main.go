bc343f1cf02d665df496293632c37be7cf715157
David Gassner <david.gassner@gmail.com>
2024-11-10 19:54:08 +0000

    End 06_01

--- Ch06/06_01e/main.go
+++ Ch06/06_01e/main.go
@@ -7,24 +7,23 @@ import (
 )
 
 func main() {
-	content := "Hello from Go!"
-	file, err := os.Create("./fromString.txt")
-	checkError(err)
+	fileName := "./fromString.txt"
+	file, err := os.Create(fileName)
 	defer file.Close()
-	length, err := io.WriteString(file, content)
 	checkError(err)
+	length, err := io.WriteString(file, "Hello from Go!")
 	fmt.Printf("Wrote a file with %v characters\n", length)
-	readFile("./fromString.txt")
-}
-
-func readFile(fileName string) {
-	data, err := os.ReadFile(fileName)
-	checkError(err)
-	fmt.Println("Text read from file:", string(data))
+	readFile(fileName)
 }
 
 func checkError(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func readFile(fileName string) {
+	data, err := os.ReadFile(fileName)
+	checkError(err)
+	fmt.Println("Text read from file:", string(data))
 }
