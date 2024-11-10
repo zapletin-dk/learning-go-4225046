b88a15fbc2bd204232f681d712bc729c1a17afcf
David Gassner <david.gassner@gmail.com>
2024-11-10 20:50:47 +0000

    Begin 06_02

--- Ch06/06_02b/main.go
+++ Ch06/06_02b/main.go
@@ -9,3 +9,9 @@ const url = "http://services.explorecalifornia.org/json/tours.php"
 func main() {
 	fmt.Println("Network requests")
 }
+
+// checkError(err error) {
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// }
--- Ch06/06_02e/main.go
+++ Ch06/06_02e/main.go
@@ -11,34 +11,30 @@ const url = "http://services.explorecalifornia.org/json/tours.php"
 func main() {
 
 	// define client using default params
-	client := http.Client{
-		Transport:     nil,
-		CheckRedirect: nil,
-		Jar:           nil,
-		Timeout:       0,
-	}
+	client := http.Client{}
+	
 	// Create request for the client to perform
 	req, err := http.NewRequest("GET", url, nil)
+	checkError(err)
+
 	// Set the User-Agent field in request header.
 	req.Header.Set("User-Agent", "")
 
-	if err != nil {
-		panic(err)
-	}
 	// Perform the request and capture the response.
 	resp, err := client.Do(req)
-
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("Response type: %T\n", resp)
+	checkError(err)
 	defer resp.Body.Close()
+	fmt.Printf("Response type: %T\n", resp)
 
 	bytes, err := io.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
+	checkError(err)
 
 	content := string(bytes)
 	fmt.Print(content)
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
