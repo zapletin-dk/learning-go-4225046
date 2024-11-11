6175982b7c1b55342c452f3e0660452bd8c5f5a8
David Gassner <david.gassner@gmail.com>
2024-11-11 20:14:23 +0000

    End 06_03

--- Ch06/06_03e/main.go
+++ Ch06/06_03e/main.go
@@ -12,16 +12,32 @@ import (
 const url = "http://services.explorecalifornia.org/json/tours.php"
 
 func main() {
-	content := ReadHttpContent()
+	content := readHttpContent()
 	// fmt.Print(content)
-	tours := toursFromJson(content)
+	tours := toursFromJSON(content)
 	for _, tour := range tours {
 		price, _ := strconv.ParseFloat(tour.Price, 16)
 		fmt.Printf("%v: ($%v)\n", tour.Name, price)
 	}
 }
 
-func ReadHttpContent() string {
+func toursFromJSON(content string) []Tour {
+
+	tours := make([]Tour, 0)
+	decoder := json.NewDecoder(strings.NewReader(content))
+	_, err := decoder.Token()
+	checkError(err)
+
+	var tour Tour
+	for decoder.More() {
+		err := decoder.Decode(&tour)
+		checkError(err)
+		tours = append(tours, tour)
+	}
+	return tours
+}
+
+func readHttpContent() string {
 	fmt.Println("Network requests")
 	client := http.Client{}
 	req, err := http.NewRequest("GET", url, nil)
@@ -39,23 +55,6 @@ func ReadHttpContent() string {
 	checkError(err)
 	return string(bytes)
 
-}
-
-func toursFromJson(content string) []Tour {
-	tours := make([]Tour, 0)
-
-	decoder := json.NewDecoder(strings.NewReader(content))
-	_, err := decoder.Token()
-	checkError(err)
-
-	var tour Tour
-	for decoder.More() {
-		err := decoder.Decode(&tour)
-		checkError(err)
-		tours = append(tours, tour)
-	}
-
-	return tours
 }
 
 func checkError(err error) {
