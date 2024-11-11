00f03aa25616a942435ba864751cdec847c2b03e
David Gassner <david.gassner@gmail.com>
2024-11-11 19:53:25 +0000

    End 06_03

--- Ch06/06_03e/main.go
+++ Ch06/06_03e/main.go
@@ -5,70 +5,63 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
 	"strings"
 )
 
 const url = "http://services.explorecalifornia.org/json/tours.php"
 
 func main() {
-
-	// define client using default params
-	client := http.Client{
-		Transport:     nil,
-		CheckRedirect: nil,
-		Jar:           nil,
-		Timeout:       0,
-	}
-	// Create request for the client to perform
-	req, err := http.NewRequest("GET", url, nil)
-	// Set the User-Agent field in request header.
-	req.Header.Set("User-Agent", "")
-
-	if err != nil {
-		panic(err)
-	}
-	// Perform the request and capture the response.
-	resp, err := client.Do(req)
-
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("Response type: %T\n", resp)
-	defer resp.Body.Close()
-
-	bytes, err := io.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	content := string(bytes)
-	fmt.Print(content)
-
+	content := ReadHttpContent()
+	// fmt.Print(content)
 	tours := toursFromJson(content)
 	for _, tour := range tours {
-		fmt.Println(tour.Name)
+		price, _ := strconv.ParseFloat(tour.Price, 16)
+		fmt.Printf("%v: ($%v)\n", tour.Name, price)
 	}
+}
+
+func ReadHttpContent() string {
+	fmt.Println("Network requests")
+	client := http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	checkError(err)
+
+	req.Header.Set("User-Agent", "")
+
+	resp, err := client.Do(req)
+	checkError(err)
+	defer resp.Body.Close()
+
+	fmt.Printf("Response type:%T\n", resp)
+
+	bytes, err := io.ReadAll(resp.Body)
+	checkError(err)
+	return string(bytes)
 
 }
 
 func toursFromJson(content string) []Tour {
-	tours := make([]Tour, 0, 20)
+	tours := make([]Tour, 0)
 
 	decoder := json.NewDecoder(strings.NewReader(content))
 	_, err := decoder.Token()
-	if err != nil {
-		panic(err)
-	}
+	checkError(err)
 
 	var tour Tour
 	for decoder.More() {
 		err := decoder.Decode(&tour)
-		if err != nil {
-			panic(err)
-		}
+		checkError(err)
 		tours = append(tours, tour)
 	}
+
 	return tours
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
 
 type Tour struct {
