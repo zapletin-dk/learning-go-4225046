92f7202d40ba865496daad86373355bd2c491583
David Gassner <david.gassner@gmail.com>
2024-11-14 18:37:24 +0000

    End 02_04e

--- Ch02/02_04e/main.go
+++ Ch02/02_04e/main.go
@@ -3,22 +3,17 @@ package main
 import "fmt"
 
 func main() {
-
 	i1, i2, i3 := 12, 45, 68
 	intSum := i1 + i2 + i3
 	fmt.Println("Integer sum:", intSum)
-
+	
 	f1, f2, f3 := 23.5, 65.1, 76.3
 	floatSum := f1 + f2 + f3
 	fmt.Println("Float sum:", floatSum)
-
+	
 	total := float64(i1) + f2
-	fmt.Println("Total:", total)
-
-	quotient := float64(i1) / f2
-	fmt.Println("Quotient:", quotient)
-
+	fmt.Println("Result:", total)
+	
 	product := float64(i1) * f2
 	fmt.Println("Product:", product)
-
 }
